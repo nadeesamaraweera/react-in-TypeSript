d2f98d78adbbabc2e1fdabc72ddfd8283ea1603b
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-11-29 10:06:34 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ButtonComponent, {SmallButtonComponent} from "./assets/components/ButtonComponent.tsx";
+
+function App() {
+
+  return (
+      <>
+          <ButtonComponent></ButtonComponent>
+          <SmallButtonComponent></SmallButtonComponent>
+
+      </>
+  )
+}
+
+export default App
---
+++ src/assets/components/ButtonComponent.tsx
@@ -0,0 +1,19 @@
+export  default function ButtonComponent() {
+    return (
+        <>
+            <button>Click Me</button>
+            <button>Click Me</button>
+            <button>Click Me</button>
+        </>
+    )
+}
+
+export function SmallButtonComponent() {
+    return (
+        <>
+            <button>Click Me1</button>
+            <button>Click Me1</button>
+            <button>Click Me1</button>
+        </>
+    )
+}
